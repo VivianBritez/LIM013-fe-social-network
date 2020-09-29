a65bea18f4541c28e42e763011b6b65465c7f856
SinaisAna <58980145+SinaisAna@users.noreply.github.com>
2020-09-29 18:04:32 -0500

    v

--- src/lib/controller/login-controller.js
+++ src/lib/controller/login-controller.js
@@ -1,19 +1,18 @@
-import { singInGoogle, readUserDB, createUserDB } from '../firebase/firestore.js';
+import { readUserDB, createUserDB } from '../firebase/firestore.js';
+import { singInGoogle, singInFacebook } from '../firebase/auth.js';
 
 const readCreateUserDB = (uid, email, photoUrl, nameUser) => {
-    readUserDB(uid)
-      .then((localStorage) => {
-        if (localStorage.empty) {
-          createUserDB(uid, email, photoUrl, nameUser);
-          
+  readUserDB(uid)
+    .then((localStorage) => {
+      if (localStorage.empty) {
+      createUserDB(uid, email, photoUrl, nameUser);
         } else {
-          localStorage.forEach((refDoc) => {
-            const user = refDoc.data();
-            
+      localStorage.forEach((refDoc) => {
+        const user = refDoc.data();  
           });
-        }
-      });
-  };
+        };
+    });
+};
 
 export const loginGoogle = () => {
     singInGoogle()
