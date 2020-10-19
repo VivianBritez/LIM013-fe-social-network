503f2f33e232a9a91d1b2abb365539ae61540ccf
SheillyR <sheillyrolo@gmail.com>
2020-10-18 22:28:23 -0500

    Added a user profile picture by default in createUser function

--- src/lib/firebase-controller/signup-controller.js
+++ src/lib/firebase-controller/signup-controller.js
@@ -1,7 +1,7 @@
 import { createUserDB } from '../firebase/firestore.js';
 import { createUserAccount, getUser } from '../firebase/auth.js';
 
-const imgProfileUserDefault = '../img/userProfile.png';
+const userProfilePicByDefault = '../img/userProfile.png';
 
 export const createUser = (email, password, name) => {
   createUserAccount(email, password)
@@ -11,11 +11,12 @@ export const createUser = (email, password, name) => {
       console.log(user);
       user.updateProfile({
         displayName: name,
+        photoURL: userProfilePicByDefault,
       });
       // Open home template
       window.location.hash = '#/home';
       // Save user data in firebase
-      createUserDB(res.user.uid, email, imgProfileUserDefault, name);
+      createUserDB(res.user.uid, email, userProfilePicByDefault, name);
     })
     .catch((error) => {
       const errorCode = error.code;
